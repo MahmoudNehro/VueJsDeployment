b529dcdf811ab87d32c7d676b9efd6b64786d9e1
MahmoudNehro <mahmoudnehro9000@gmail.com>
2023-01-21 16:21:47 +0200

    base url

--- src/http-requests.js
+++ src/http-requests.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://scandi-web-task.000webhostapp.com",
+  baseURL: "http://onlinecrudapimahmoud.medianewsonline.com",
   headers: {
     "Content-type": "application/json",
   }
